a11167d314bf01fe219cca4ad6ddef71fa995825
EthanPDF <ethangreaves7@gmail.com>
2026-06-07 17:45:25 +0200

    Game and Battle headers finished

--- battle.h
+++ battle.h
@@ -1,10 +1,19 @@
 #ifndef BATTLE_H
 #define BATTLE_H
+#include "character.h"
+#include "monster.h"
 
 class Battle
 {
 public:
     Battle();
+    void startBattle(Character c, Monster m);
+
+    void chooseTurn();
+    void attackPhase();
+
+    bool isDefeated();
+    bool monsterDefeated();
 };
 
 #endif // BATTLE_H
--- game.h
+++ game.h
@@ -1,10 +1,26 @@
 #ifndef GAME_H
 #define GAME_H
+#include <vector>
+#include "character.h"
+#include "monster.h"
 
 class Game
 {
 public:
     Game();
+    bool startWithMonsters();
+
+    void createCharacter();
+    void leave();
+    void fight();
+
+    Monster chooseMonster();
+    bool allMonstersDefeated();
+
+private:
+    Character Player;
+    std::vector<Monster> MonstersInGame;
+
 };
 
 #endif // GAME_H
