a699870d6373d4244e30b2c31d062047cb56025c
EthanPDF <ethangreaves7@gmail.com>
2026-06-07 15:46:09 +0200

    Monsters commit

--- monsters.h
+++ monsters.h
@@ -1,10 +1,15 @@
 #ifndef MONSTERS_H
 #define MONSTERS_H
+#include <string>
 
 class Monsters
 {
 public:
     Monsters();
+
+private:
+    std::string Name;
+    int HP, Strength;
 };
 
 #endif // MONSTERS_H
