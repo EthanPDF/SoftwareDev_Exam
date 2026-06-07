e1fe0268149d92397576235e4cc629bd081d05df
EthanPDF <ethangreaves7@gmail.com>
2026-06-07 15:21:56 +0200

    Initial commit

---
+++ build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_SoftwareDev_Exam_FILE C:/Users/derpy/OneDrive/Documents/SoftwareDev_Exam/build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/SoftwareDev_Exam.exe)
+set(__QT_DEPLOY_TARGET_SoftwareDev_Exam_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_SoftwareDev_Exam_RUNTIME_DLLS C:/Qt/6.10.2/mingw_64/bin/Qt6Core.dll)
