8bd97ada39b882897b65eaff0851a9891e387853
EthanPDF <ethangreaves7@gmail.com>
2026-06-07 15:45:41 +0200

    Character commit

--- character.h
+++ character.h
@@ -1,10 +1,20 @@
 #ifndef CHARACTER_H
 #define CHARACTER_H
+#include <vector>
+#include "monsters.h"
 
 class Character
 {
 public:
     Character();
+    void attack(Monsters m);
+    void takDamage(int dmg);
+    void addMonster(Monsters m);
+    void replaceMonster(Monsters m);
+
+private:
+    int HP, Strength;
+    std::vector<Monsters> PlayerMonsters;
 };
 
 #endif // CHARACTER_H
